084e32b2c2369fece7be37f8df3b907cf9212357
lunatictiol <bhatsabiq9@gmail.com>
2024-11-16 10:23:57 +0530

    mail service impl

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -20,7 +20,23 @@ func (app *Config) routes() http.Handler {
 	mux.Get("/register", app.RegisterPage)
 	mux.Post("/register", app.PostRegister)
 	mux.Get("/activate-account", app.ActivateAccount)
-	
+	mux.Get("/test-mail", func(w http.ResponseWriter, r *http.Request) {
+		m := Mail{
+			Domain:      "localhost",
+			Host:        "local",
+			Port:        1025,
+			Encryption:  "none",
+			FromAddress: "test@test.com",
+			FromName:    "test",
+			ErrorChan:   make(chan error),
+		}
+		mail := Message{
+			To:      "me@test.com",
+			Subject: "test",
+			Data:    "test hello",
+		}
+		m.sendMail(mail, make(chan error))
+	})
 
 	return mux
 }
